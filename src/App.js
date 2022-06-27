1893b827846a017f8532cfe29405a3a45ac185a2
kaxadontstarve <103281248+kaxadontstarve@users.noreply.github.com>
2022-06-28 00:01:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./components/Footer";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
